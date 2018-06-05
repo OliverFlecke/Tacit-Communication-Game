e5e1133266963cfaec2ca856a5a55e72904bb100
Oliver Fleckenstein <Oliverfl@live.dk>
2018-06-05 13:05:40 +0200

    Logging path

--- src/Game.tsx
+++ src/Game.tsx
@@ -6,23 +6,30 @@ import { Player } from './Player';
 
 interface IGameState {
     position: Location;
+    path: Action[];
 }
 
 export class Game extends React.Component<{}, IGameState> {
 
-    public table;
+    private readonly gameSize = 3;
 
     public constructor(props) {
         super(props);
         this.state = {
-            position: new Location(2, 2)
+            position: new Location(2, 2),
+            path: []
         }
+    }
+
+    // tslint:disable-next-line:no-empty
+    componentDidUpdate() {
+
     }
 
     /**
      * KeyboardInput
      */
-    public onKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
+    private onKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
         switch (event.key) {
             case 'ArrowRight':
                 this.UpdateLocation(Action.Right, Player.Sender);
@@ -41,30 +48,57 @@ export class Game extends React.Component<{}, IGameState> {
         }
     }
 
-    public UpdateLocation(action: Action, player: Player) {
+    private IsValidAction(action: Action, player: Player): boolean {
+        const position = this.state.position;
+        switch (action) {
+            case Action.Right:  return position.x < this.gameSize;
+            case Action.Left:   return position.x > 1;
+            case Action.Up:     return position.y > 1;
+            case Action.Down:   return position.y < this.gameSize;
+            default:            return false;
+        }
+    }
+
+    private UpdateLocation(action: Action, player: Player) {
+        if (!this.IsValidAction(action, player)) {
+            return;
+        }
+
         const position: Location = this.state.position;
         switch (action) {
             case Action.Down:
-                position.y = Math.min(3, position.y + 1);
+                position.y = position.y + 1;
                 break;
             case Action.Up:
-                position.y = Math.max(1, position.y - 1);
+                position.y = position.y - 1;
                 break;
             case Action.Left:
-                position.x = Math.max(1, position.x - 1);
+                position.x = position.x - 1;
                 break;
             case Action.Right:
-                position.x = Math.min(3, position.x + 1);
+                position.x = position.x + 1;
             default:
                 break;
         }
+        const path = this.state.path.concat(action);
         this.setState({
-            position
+            position,
+            path
         });
     }
 
+    private FinishTurn(event: React.MouseEvent<HTMLButtonElement>) {
+        console.log('finish turn');
+    }
 
-    public render() {
+    private GetCoordinates(player: Player): CSSProperties {
+        return {
+            gridColumnStart: this.state.position.x,
+            gridRowStart: this.state.position.y
+        }
+    }
+
+    private renderGrid() {
         return (
             <div
                 className='game-container'
@@ -87,17 +121,19 @@ export class Game extends React.Component<{}, IGameState> {
                 >
                     <span
                         className='square'
-                        style={this.getCoordinates(Player.Sender)}
+                        style={this.GetCoordinates(Player.Sender)}
                     />
                 </div>
             </div>
         );
     }
 
-    public getCoordinates(player: Player): CSSProperties {
-        return {
-            gridColumnStart: this.state.position.x,
-            gridRowStart: this.state.position.y
-        }
+    public render() {
+        return (
+            <div>
+                {this.renderGrid()}
+                <button onClick={this.FinishTurn}>Finish</button>
+            </div>
+        );
     }
 }
