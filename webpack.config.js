c96657bee88552546d0089239e777bc203530684
Oliver Fleckenstein <Oliverfl@live.dk>
2018-06-10 13:31:04 +0200

    Publishing game to GitHub pages now work

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
     mode: "production",
     entry: "./src/index.tsx",
     output: {
-        filename: "bundle.js",
+        filename: "app.bundle.js",
         path: __dirname + "/dist"
     },
 
@@ -33,12 +33,12 @@ module.exports = {
         ]
     },
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    }
+    // // When importing a module whose path matches one of the following, just
+    // // assume a corresponding global variable exists and use that instead.
+    // // This is important because it allows us to avoid bundling all of our
+    // // dependencies, which allows browsers to cache those libraries between builds.
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // }
 };
